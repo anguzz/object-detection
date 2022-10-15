b5a002b7afcad961421d75de395bd21264b1f0d2
Angel <santoyox714@gmail.com>
2022-10-15 03:52:46 -0700

    add tensor imports, make variables for cam/stream

--- src/lib/pages/Content.svelte
+++ src/lib/pages/Content.svelte
@@ -1,16 +1,31 @@
-<script lang="ts">
+<script>
   import Link from "$lib/components/Link.svelte";
   import Page from "$lib/components/Page.svelte";
   import Text from "$lib/components/Text.svelte";
   import { primaryBackground } from "$lib/utils/constants";
 
   export let backgroundClass = primaryBackground;
+  import {onMount} from "svelte";
+
+  import '@tensorflow/tfjs';
+  import * as cocossd from "@tensorflow-models/coco-ssd";
+
+
+
+  let promise;
+  let model;
+  let webcam;
+  let video;
+  let classify;
+
+ 
 </script>
 
 <Page id="content" title=" " {backgroundClass}>
   <Text>
-    Content!
-  </Text>
+  
+     
+     </Text>
 </Page>
 
 
