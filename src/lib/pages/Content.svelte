c3001808f11ea72f36bc3c940eee6c57cdd41cd7
Angel <santoyox714@gmail.com>
2022-10-15 04:11:30 -0700

    Update Content.svelte

--- src/lib/pages/Content.svelte
+++ src/lib/pages/Content.svelte
@@ -3,14 +3,10 @@
   import Page from "$lib/components/Page.svelte";
   import Text from "$lib/components/Text.svelte";
   import { primaryBackground } from "$lib/utils/constants";
-
-  export let backgroundClass = primaryBackground;
   import {onMount} from "svelte";
-
   import '@tensorflow/tfjs';
   import * as cocossd from "@tensorflow-models/coco-ssd";
-
-
+  export let backgroundClass = primaryBackground;
 
   let promise;
   let model;
